c840d28487bc101ea750fd1e79ed670ee32e1d4c
BartoszCiaston <BartoszCiaston1@gmail.com>
2020-01-15 13:14:32 +0100

    Add Can_Add_Quantity_For_Existing_Lines test to CartTests

--- SportsStore.Tests/CartTests.cs
+++ SportsStore.Tests/CartTests.cs
@@ -12,11 +12,11 @@ namespace SportsStore.Tests
         [Fact]
         public void Can_Add_New_Lines()
         {
-            //Arrange  - make testing products.
+            //Arrange  - create test products.
             Product p1 = new Product { ProductID = 1, Name = "P1" };
             Product p2 = new Product { ProductID = 2, Name = "P2" };
 
-            //Arrange - creating cart.
+            //Arrange - create cart.
             Cart target = new Cart();
 
             //Act.
@@ -24,10 +24,32 @@ namespace SportsStore.Tests
             target.AddItem(p2, 1);
             CartLine[] results = target.Lines.ToArray();
 
-            //Assert.
+            //Asserts.
             Assert.Equal(2, results.Length);
             Assert.Equal(p1, results[0].Product);
             Assert.Equal(p2, results[1].Product);
+        }
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //Arrange - create test products.
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+           
+            //Arrange - create cart.
+            Cart target = new Cart();
+
+            //Act.
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+
+            //Asserts.
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
         }
     }
 }
