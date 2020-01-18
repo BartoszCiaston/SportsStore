6c506c889da187681e83a521cf26f64027065ab9
BartoszCiaston <BartoszCiaston1@gmail.com>
2020-01-18 22:02:32 +0100

    Add Cannot_Edit_Nonexistent_Product test in AdminControllerTests

--- SportsStore.Tests/AdminControllerTests.cs
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -63,6 +63,27 @@ namespace SportsStore.Tests
             Assert.Equal(3, p3.ProductID);
         }
 
+        [Fact]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            //Arrange - create repository imitation.
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"}
+            }.AsQueryable<Product>());
+
+            //Arrange - create controller.
+            AdminController target = new AdminController(mock.Object);
+
+            //Act.
+            Product result = GetViewModel<Product>(target.Edit(4));
+
+            Assert.Null(result);
+        }
+
         private T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
